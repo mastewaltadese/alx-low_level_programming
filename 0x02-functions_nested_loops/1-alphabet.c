0cb94bac0dba51a2b1a9ef1b831395fc7648c8af
mastewaltadese <mastewaltadese7@gmail.com>
2022-12-13 07:24:03 -0800

    edited

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * main - check the code
+ * print_alphabet - check the code
  * Return: Always 0.
  */
 void print_alphabet(void)
@@ -11,5 +11,6 @@ void print_alphabet(void)
 	_putchar(c);
 	c++;
 	}
+
 	_putchar('\n');
 }
