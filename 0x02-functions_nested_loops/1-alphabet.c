480812bfc67a0760839c3bace72281b91f5fa33b
mastewaltadese <mastewaltadese7@gmail.com>
2022-12-13 07:05:18 -0800

    edited

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main"
+/**
+ * main - check the code
+ * Return: Always 0.
+ */
+void print_alphabet(void)
+{
+	char c = 'a';
+	while (c <= 'z')
+	{
+		_putchar(c);
+		c++;
+	}
+	_putchar('\n');
