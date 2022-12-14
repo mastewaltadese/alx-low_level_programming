9aa32367698c184dcdbc4b65d0287dd11c565e38
mastewaltadese <mastewaltadese7@gmail.com>
2022-12-14 03:55:20 -0800

    sign

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#includ "main.h"
+/**
+ * _abs - computes the ab value of int
+ * @n: the int in question
+ * return: the abs of the int.
+ */
+int _abs(int n)
+{
+	if (n >= 0)
+		return (n);
+	else
+		return (-n);
+}
+
