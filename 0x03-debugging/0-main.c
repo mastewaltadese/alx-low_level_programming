6943357b874b32d08934989df5a05402d90bd26c
mastewaltadese <mastewaltadese7@gmail.com>
2022-12-15 04:44:36 -0800

    so what

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,15 @@
+~#include "main.h"
+
+/**
+ * main - test function for posetive or negative
+ * return: 0
+ */
+int main(void)
+{
+	int i;
+
+	i = 0;
+	posetive_or_negative(i);
+
+	return (0);
+}
---
+++ 0x03-debugging/README.md
@@ -0,0 +1,1 @@
+thes debug the code
