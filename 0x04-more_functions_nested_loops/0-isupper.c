cbc60c4c796890e376366de0c7c72c2efa2aa440
mastewaltadese <mastewaltadese7@gmail.com>
2022-12-17 08:21:25 -0800

    0. isupper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ *_isupper- cheches if c is upercase
+ *@c: a caracter to be checked
+ *Return: Always 0
+ */
+int _isupper(int c)
+{
+	int n = isupper(0);
+	
+	if (n == 1)
+	{
+		_putchar('1');
+	}
+	else
+	{
+		_putchar('0');
+	}
+	return (0);
+}
+
